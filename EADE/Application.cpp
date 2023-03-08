afcc5f66cb3b524da18647137453611a8005b37c
unknown <bachinge@technikum-wien.at>
2023-03-08 12:31:15 +0100

    Engine Events, Logging from last week has been ommited

--- EADE/Application.cpp
+++ EADE/Application.cpp
@@ -1,9 +1,23 @@
 #include "Application.h"
+#include "Event.h"
+#include "ApplicationEvent.h"
+#include <iostream>
 
 namespace EADE
 {
 	void Application::run()
 	{
+		WindowResizeEvent e(1200, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			std::cout << e.ToString() << std::endl;
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			std::cout << e.ToString() << std::endl;
+		}
+		
+		//Logging
 		while (true);
 	}
 }
--- EADE/EADE.h
+++ EADE/EADE.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
 #include "Application.h"
 #include "EntryPoint.h"
